f5e65eadeceb6d6fc03dc145372fd5e6cb5bb5f7
mikk8718 <baunsgaard38@gmail.com>
2022-03-01 11:28:15 +0100

    Revert "Created Logging System"
    This reverts commit 232d9ac226595a89629a6c64f1df982b623ce1a9.

--- P8_VR_Home/Source/P8_VR_Home/P8_VR_Home.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "P8_VR_Home.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, P8_VR_Home, "P8_VR_Home" );
